89e18c91c1b865b75ce689732a22677f4389a99d
isuruAb <vpisabeywardana@gmail.com>
2017-09-15 12:28:07 +0530

    add readme file

--- tests/index-test.js
+++ tests/index-test.js
@@ -15,9 +15,9 @@ describe('Component', () => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
+  it('sample test case', () => {
     render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+      expect(node.innerHTML).toContain('Prev')
     })
   })
 })
